df660e887c254723e95a0256b8863060fb63597b
criksman <alvarojg2002@hotmail.com>
2023-12-04 10:44:30 -0300

    Mostrar progreso ajustado.

--- resources/views/user/comenzar/show_resultado.blade.php
+++ resources/views/user/comenzar/show_resultado.blade.php
@@ -7,9 +7,8 @@
         <div class="col-md-8 offset-md-2">
             <div class="card mt-5">
                 <div class="card-body text-center shadow">
-                    <h1 class="card-title">Resultado: {{$porcentaje}}</h1>
+                    <h1 class="card-title">Resultado: {{$porcentaje}}%</h1>
                     <p class="card-text">¡Has completado el test!</p>
-                    <p class="card-text">Aquí está tu resultado: {{$porcentaje}}%</p>
                     <a href="{{ route('user.comenzar.formulario', $tematica->tematica_id) }}" class="btn btn-primary mt-3"> Volver </a>
                 </div>
             </div>
